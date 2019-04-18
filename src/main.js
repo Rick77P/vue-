83c130bc3114ab2064f30df307afe9c03ee3b24e
Rick <877979404@qq.com>
2019-04-18 20:19:48 +0800

    初始文件

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },//注册组件之后就可以直接标签化使用了。
+  template: '<App/>'
+})
